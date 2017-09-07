a65c107672898cdcde582c5fc164884bc74078d2
Sebastian Pech <sebastian.pech@me.com>
2017-09-07 08:13:21 +0200

    Allow Gtk >= 3.10

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,4 @@
 include("test_actorsworld.jl")
 include("test_kara_01.jl")
 include("test_xml.jl")
-using Gtk
-Gtk.libgtk_version >= v"3.20" && include("test_kara_gui.jl")
+include("test_kara_gui.jl")
